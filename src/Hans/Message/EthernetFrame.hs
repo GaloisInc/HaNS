a3f7b373ead84359b89a0825af74c008c106a4f3
Trevor Elliott <trevor@galois.com>
2012-01-11 14:51:10 -0800

    Don't rely on Serialize instances for ethernet frames

--- src/Hans/Message/EthernetFrame.hs
+++ src/Hans/Message/EthernetFrame.hs
@@ -5,20 +5,33 @@ module Hans.Message.EthernetFrame (
   , EthernetFrame(..)
   ) where
 
-import Hans.Address.Mac (Mac)
+import Hans.Address.Mac (Mac,parseMac,renderMac)
 
+import Control.Applicative ((<$>),(<*>))
 import Data.Serialize (Serialize(..))
-import Data.Serialize.Get (Get,getBytes,remaining)
-import Data.Serialize.Put (Put,putByteString)
+import Data.Serialize.Get (Get,getBytes,remaining,getWord16be)
+import Data.Serialize.Put (Putter,putByteString,putWord16be)
 import Data.ByteString (ByteString)
 import Data.Word (Word16)
 import Numeric (showHex)
 
+
+-- Ether Type ------------------------------------------------------------------
+
 newtype EtherType = EtherType { getEtherType :: Word16 }
-  deriving (Eq,Num,Ord,Serialize)
+  deriving (Eq,Num,Ord)
 
 instance Show EtherType where
   showsPrec _ (EtherType et) = showString "EtherType 0x" . showHex et
+
+parseEtherType :: Get EtherType
+parseEtherType  = EtherType <$> getWord16be
+
+renderEtherType :: Putter EtherType
+renderEtherType  = putWord16be . getEtherType
+
+
+-- Ethernet Frames -------------------------------------------------------------
 
 data EthernetFrame = EthernetFrame
   { etherDest   :: !Mac
@@ -32,13 +45,16 @@ instance Serialize EthernetFrame where
   put = renderEthernetFrame
 
 parseEthernetFrame :: Get EthernetFrame
-parseEthernetFrame  = do
-  dst  <- get
-  src  <- get
-  ty   <- get
-  body <- getBytes =<< remaining
-  return $! EthernetFrame dst src ty body
+parseEthernetFrame
+   =  EthernetFrame
+  <$> parseMac
+  <*> parseMac
+  <*> parseEtherType
+  <*> (getBytes =<< remaining)
 
-renderEthernetFrame :: EthernetFrame -> Put
-renderEthernetFrame (EthernetFrame s d t da) =
-  put s >> put d >> put t >> putByteString da
+renderEthernetFrame :: Putter EthernetFrame
+renderEthernetFrame frame = do
+  renderMac        (etherDest   frame)
+  renderMac        (etherSource frame)
+  renderEtherType  (etherType   frame)
+  putByteString    (etherData   frame)
