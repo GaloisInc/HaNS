e81c74306e9abcbbc1371bcfb7f5aa798cbeb6c7
Trevor Elliott <trevor@galois.com>
2012-08-21 13:29:28 -0700

    Experimental receive window implementation

--- src/Hans/Layer/Tcp/Window.hs
+++ src/Hans/Layer/Tcp/Window.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module Hans.Layer.Tcp.Window (
     module Hans.Layer.Tcp.Window
   , module Hans.Layer.Tcp.WaitBuffer
@@ -7,6 +9,7 @@ import Hans.Layer.Tcp.WaitBuffer
 import Hans.Message.Tcp
 
 import Control.Monad (mzero)
+import Data.Function (on)
 import Data.Time.Clock.POSIX (POSIXTime)
 import Data.Monoid (Sum(..))
 import Data.Word (Word16)
@@ -109,3 +112,79 @@ decrementRTO seg
   | otherwise        = seg'
   where
   seg' = seg { segRTO = segRTO seg - 1 }
+
+
+-- Local Window ----------------------------------------------------------------
+
+-- | Local window, containing a buffer of incoming packets, indexed by their 
+data LocalWindow = LocalWindow
+  { lwBuffer :: Seq.Seq InSegment
+  , lwRcvNxt :: !TcpSeqNum
+  } deriving (Show)
+
+-- | Empty local buffer, with an initial sequence number as the next expected
+-- sequence number.
+emptyLocalWindow :: TcpSeqNum -> LocalWindow
+emptyLocalWindow sn = LocalWindow
+  { lwBuffer = Seq.empty
+  , lwRcvNxt = sn
+  }
+
+setRcvNxt :: TcpSeqNum -> LocalWindow -> LocalWindow
+setRcvNxt sn lw = lw { lwRcvNxt = sn }
+
+inOrder :: InSegment -> LocalWindow -> Bool
+inOrder seg lw = isSeqNum seg == lwRcvNxt lw
+
+-- | Handle an incoming segment.
+incomingPacket :: InSegment -> LocalWindow -> (Seq.Seq InSegment, LocalWindow)
+incomingPacket is lw
+  | inOrder is lw = (is Seq.<| segs, lw')
+  | otherwise     = (Seq.empty, bufferSegment is lw)
+  where
+  (segs,lw') = dequeueSegments (setRcvNxt (isRcvNxt is) lw)
+
+-- | Dequeue segments from the buffer while they're contiguous.
+dequeueSegments :: LocalWindow -> (Seq.Seq InSegment, LocalWindow)
+dequeueSegments  = loop Seq.empty
+  where
+  loop segs lw = case dequeueSegment lw of
+    Just (is,lw') -> loop (segs Seq.|> is) lw'
+    Nothing       -> (segs,lw)
+
+-- | Try to dequeue a contiguous segment from the local window.
+dequeueSegment :: LocalWindow -> Maybe (InSegment,LocalWindow)
+dequeueSegment lw = case Seq.viewl (lwBuffer lw) of
+  a Seq.:< rest
+    | inOrder a lw -> Just (a, lw { lwBuffer = rest, lwRcvNxt = isRcvNxt a })
+  _                -> Nothing
+
+bufferSegment :: InSegment -> LocalWindow -> LocalWindow
+bufferSegment is lw = lw { lwBuffer = insert (lwBuffer lw) }
+  where
+  sn         = isSeqNum is
+  insert buf = case Seq.viewl buf of
+
+    a Seq.:< rest | sn > isSeqNum a -> a  Seq.<| insert rest
+                  | otherwise       -> is Seq.<| buf
+    Seq.EmptyL                      -> Seq.singleton is
+
+data InSegment = InSegment
+  { isRcvNxt :: !TcpSeqNum -- ^ @TcpSeqNum@ to follow
+  , isWrap   :: Bool
+  , isHeader :: !TcpHeader
+  , isBody   :: !L.ByteString
+  } deriving (Show)
+
+isSeqNum :: InSegment -> TcpSeqNum
+isSeqNum  = tcpSeqNum . isHeader
+
+mkInSegment :: TcpHeader -> L.ByteString -> InSegment
+mkInSegment hdr body = InSegment
+  { isRcvNxt = rcvNxt
+  , isWrap   = tcpSeqNum hdr > rcvNxt -- should be rare
+  , isHeader = hdr
+  , isBody   = body
+  }
+  where
+  rcvNxt = tcpSeqNum hdr + fromIntegral (L.length body)
