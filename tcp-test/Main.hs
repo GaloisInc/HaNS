ec99cb2a3f4fee35afb2203bcb708c9dbedce3f5
Adam Wick <awick@galois.com>
2014-05-09 17:57:32 -0700

    tcp-test now works with the HaLVM.

--- tcp-test/Main.hs
+++ tcp-test/Main.hs
@@ -1,14 +1,22 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 import Hans.Address
 import Hans.DhcpClient
 import Hans.Address.Mac
 import Hans.Address.IP4
-import Hans.Device.Tap
 import Hans.NetworkStack
+
+#ifdef HaLVM_HOST_OS
+import Hans.Device.Xen
+import Hypervisor.Console
+import Hypervisor.XenStore
+import XenDevice.NIC
+#else
+import Hans.Device.Tap
+#endif
 
 import Control.Concurrent (newEmptyMVar,putMVar,takeMVar,threadDelay,forkIO
                           ,killThread,myThreadId)
@@ -51,6 +59,7 @@ server ns = do
   forever $ do
     putStrLn "accepting"
     conn <- accept sock
+    putStrLn "accepted"
     _ <- forkIO $ do
       putStrLn ("Got one: " ++ show (sockRemoteHost conn))
       forever $ do
@@ -69,14 +78,29 @@ message  = "Hello, world\n"
 sleep :: Int -> IO ()
 sleep s = threadDelay (s * 1000 * 1000)
 
+setAddress :: Mac -> NetworkStack -> IO ()
+setAddress mac ns = do
+  addIP4Addr ns (localAddr `withMask` 24) mac 1500
+  routeVia ns (IP4 0 0 0 0 `withMask` 0) (IP4 192 168 90 1)
+
+#ifdef HaLVM_HOST_OS
+initEthernetDevice :: NetworkStack -> IO Mac
+initEthernetDevice ns =
+  do xs <- initXenStore
+     _ <- initXenConsole -- should set up putStrLn
+     nics <- listNICs xs
+     case nics of
+       [] -> fail "No NICs found to use!"
+       (macstr:_) ->
+         do let mac = read macstr
+            nic <- openNIC xs macstr 
+            addDevice ns mac (xenSend nic) (xenReceiveLoop nic)
+            return mac
+#else
 initEthernetDevice :: NetworkStack -> IO Mac
 initEthernetDevice ns = do
   let mac = Mac 0x52 0x54 0x00 0x12 0x34 0x56
   Just dev <- openTapDevice "tap6"
   addDevice ns mac (tapSend dev) (tapReceiveLoop dev)
   return mac
-
-setAddress :: Mac -> NetworkStack -> IO ()
-setAddress mac ns = do
-  addIP4Addr ns (localAddr `withMask` 24) mac 1500
-  routeVia ns (IP4 0 0 0 0 `withMask` 0) (IP4 192 168 90 1)
+#endif
