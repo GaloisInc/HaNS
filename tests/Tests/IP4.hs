fbecbec6eb90fe6e150487962091d37d1a3423ff
Trevor Elliott <trevor@galois.com>
2015-11-16 18:32:21 -0800

    Icmp4 Packet tests

--- tests/Tests/IP4.hs
+++ tests/Tests/IP4.hs
@@ -1,7 +1,8 @@
 module Tests.IP4 (ip4Tests) where
 
-import Tests.IP4.Fragmentation
-import Tests.IP4.Packet
+import Tests.IP4.Fragmentation (fragTests)
+import Tests.IP4.Icmp4 (icmp4Tests)
+import Tests.IP4.Packet (packetTests)
 
 import Test.Tasty
 
@@ -10,4 +11,5 @@ ip4Tests :: TestTree
 ip4Tests  = testGroup "IP4"
   [ packetTests
   , fragTests
+  , icmp4Tests
   ]
--- tests/Tests/Utils.hs
+++ tests/Tests/Utils.hs
@@ -9,5 +9,5 @@ encodeDecodeIdentity :: (Eq a, Show a) => Putter a -> Get a -> Gen a -> Property
 encodeDecodeIdentity put get gen =
   forAll gen $ \ a ->
     case runGet get (runPut (put a)) of
-      Right a'  -> label "Serialization" (a === a')
-      Left str  -> label "Decode error" False
+      Right a'  -> a === a'
+      Left str  -> property False
