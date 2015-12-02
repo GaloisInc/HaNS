6618f62b94f612202ed4c870c538cd914e5f56d2
Trevor Elliott <trevor@galois.com>
2015-12-02 09:36:25 -0800

    Remove some debugging output

--- src/Hans/Udp/Input.hs
+++ src/Hans/Udp/Input.hs
@@ -23,9 +23,6 @@ processUdp4 ns dev src dst bytes =
                                      $ ip4PseudoHeader src dst IP4_PROT_UDP
                                      $ S.length bytes
 
-
-     io (print ("cs",checksum))
-
      unless (dcChecksumOffload (devConfig dev) || checksum == 0)
          (dropPacket (devStats dev))
 
