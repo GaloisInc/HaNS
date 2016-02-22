b4ad861cb3d8d99d4cf7af03986bc3ac4f6107ef
Trevor Elliott <trevor@galois.com>
2016-02-22 13:53:10 -0800

    Make `create-tap.sh` a little more flexible

--- create-tap.sh
+++ create-tap.sh
@@ -2,6 +2,11 @@
 
 set -ev
 
+bridge=$1
+if [ -z "$bridge" ]; then
+	bridge=xenbr0
+fi
+
 sudo ip tuntap add dev tap1 mode tap user $USER
-sudo ip link set dev tap1 master xenbr0
+sudo ip link set dev tap1 master $bridge
 sudo ip link set dev tap1 up
