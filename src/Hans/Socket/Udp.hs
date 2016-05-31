023a0a993a2a7b35780f08d59ce29f992f19bf4a
Michael Hueschen <mhueschen@galois.com>
2016-05-31 14:58:51 -0700

    Expose recvfrom', which return dst addr

--- src/Hans/Socket/Udp.hs
+++ src/Hans/Socket/Udp.hs
@@ -126,22 +126,28 @@ instance DataSocket UdpSocket where
          Just (_,buf) -> return $! Just $! L.fromStrict $! S.take len buf
          Nothing      -> return Nothing
 
-
-
 -- | Receive, with information about who sent this datagram.
 recvfrom :: Network addr
          => UdpSocket addr -> IO (Device,addr,SockPort,L.ByteString)
-recvfrom UdpSocket { .. } = loop
+recvfrom sock = do
+  (dev,srcAddr,srcPort,_,msg) <- recvfrom' sock
+  return (dev,srcAddr,srcPort,msg)
+{-# INLINE recvfrom #-}
+
+recvfrom' :: Network addr
+         => UdpSocket addr -> IO (Device,addr,SockPort,addr,L.ByteString)
+recvfrom' UdpSocket { .. } = loop
   where
 
   -- NOTE: this loop shouldn't run more than one time, as it's very unlikely
   -- that we receive a packet destined for a different protocol address
   loop =
-    do ((dev,addr,srcPort,_,_), chunk) <- DGram.readChunk udpBuffer
-       case fromAddr addr of
-         Just src -> return (dev,src,srcPort,L.fromStrict chunk)
-         Nothing  -> loop
-{-# INLINE recvfrom #-}
+    do ((dev,srcAddr,srcPort,dstAddr,_), chunk) <- DGram.readChunk udpBuffer
+       case (fromAddr srcAddr, fromAddr dstAddr) of
+         (Just srcAddr', Just dstAddr') ->
+           return (dev,srcAddr',srcPort,dstAddr',L.fromStrict chunk)
+         _ -> loop
+{-# INLINE recvfrom' #-}
 
 
 -- | Send to a specific end host.
